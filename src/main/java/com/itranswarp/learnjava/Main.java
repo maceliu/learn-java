17ab7fe5c4be3b528e6395360daa9751c7a6ef3f
maceliu <465564927@qq.com>
2020-07-22 15:52:36 +0800

    1

--- src/main/java/com/itranswarp/learnjava/Main.java
+++ src/main/java/com/itranswarp/learnjava/Main.java
@@ -1,4 +1,21 @@
 package com.itranswarp.learnjava;
 
+
+import org.apache.catalina.startup.Tomcat;
+
+import java.io.File;
+
 public class Main {
+	// 启动Tomcat:
+	Tomcat tomcat = new Tomcat();
+        tomcat.setPort(Integer.getInteger("port", 8080));
+        tomcat.getConnector();
+	// 创建webapp:
+	Context ctx = tomcat.addWebapp("", new File("src/main/webapp").getAbsolutePath());
+	WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(
+				new DirResourceSet(resources, "/WEB-INF/classes", new File("target/classes").getAbsolutePath(), "/"));
+        ctx.setResources(resources);
+        tomcat.start();
+        tomcat.getServer().await();
 }
